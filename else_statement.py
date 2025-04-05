8be928dbc750b495fcac47ab2dc6d92af0fb1978
NaughtyPickl3s <crgrei99@gmail.com>
2025-04-05 16:06:18 -0400

    Add files via upload

---
+++ else_statement.py
@@ -0,0 +1,9 @@
+import numpy as nb
+#print("Hello welcome to my number calculator\nIf you need to find out whether a number is real or not, enter it below")
+core_length = input("Please enter a length for reactor core")
+#Takes the measurement of the length of the reactor core
+num_points = input("Please enter the total number of grid points for rods")
+#This is the number of individual subspaces that the core will be divided into for a point based calculation
+
+
+core_temperature = 
